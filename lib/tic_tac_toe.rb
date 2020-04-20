8f3b4a2475417d48752f02d91661e7a0442f3166
masterful-array-3850 <crgammage@gmail.com>
2020-04-20 19:35:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -119,7 +119,7 @@ def play
   end
   if won?
     winner == "X" || winner == "O"
-    puts "Congratulations #{{winner}}!"
+    puts "Congratulations #{winner}!"
   elsif draw?
     puts "Cat's Game!"
   end
